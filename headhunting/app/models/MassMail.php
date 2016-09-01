2018b772c4b7db6d49fb92d4e965a31b25567686
Nakul Suneja <nakul.suneja@live.com>
2016-09-02 02:46:42 +0530

    changes for Massmail and sql changes for massmail.

---
+++ headhunting/app/models/MassMail.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * MassMail.php
+ *
+ * This file contatins Model class to provide Data Logic for all Client Table
+ *
+ * @category   Models
+ * @package    MassMail
+ * @version    SVN: <svn_id>
+ * @since      02nd Sept 2016
+ */
+
+/**
+ * Model class to provide Data Logic
+ *
+ * @category   Models
+ * @package    MassMail
+ *
+ */
+class MassMail extends Eloquent {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'mass_mails';
+}
