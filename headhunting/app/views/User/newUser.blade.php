f4671e210b967a67030190551637d4dfd44ad7b1
BillWill <developer@hopsync.us>
2016-09-24 02:10:01 +0530

    changes in user view.

--- headhunting/app/views/User/newUser.blade.php
+++ headhunting/app/views/User/newUser.blade.php
@@ -8,7 +8,7 @@
         {{ Form::label('email', 'E-Mail: ', array('class' => 'col-sm-3
         control-label')); }}
         <div class="col-sm-8">{{ Form::text('email', "", array('class' =>
-            'form-control', 'placeholder' => 'Enter Your Email')); }} 
+            'form-control', 'placeholder' => 'Enter Your Email')); }}
             <span class='errorlogin email-login'>{{$errors->first('email');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
@@ -17,7 +17,7 @@
         {{ Form::label('first_name', 'First Name: ', array('class' => 'col-sm-3
         control-label')); }}
         <div class="col-sm-8">{{ Form::text('first_name', "", array('class' =>
-            'form-control', 'placeholder' => 'Enter Employee First Name')); }} 
+            'form-control', 'placeholder' => 'Enter Employee First Name')); }}
             <span class='errorlogin email-login'>{{$errors->first('first_name');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
@@ -25,7 +25,7 @@
         {{ Form::label('last_name', 'Last Name: ', array('class' => 'col-sm-3
         control-label')); }}
         <div class="col-sm-8">{{ Form::text('last_name', "", array('class' =>
-            'form-control', 'placeholder' => 'Enter Employee Last Name')); }} 
+            'form-control', 'placeholder' => 'Enter Employee Last Name')); }}
             <span class='errorlogin email-login'>{{$errors->first('last_name');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
@@ -34,15 +34,15 @@
         {{ Form::label('designation', 'Designation: ', array('class' => 'col-sm-3
         control-label')); }}
         <div class="col-sm-8">{{ Form::text('designation', "", array('class' =>
-            'form-control', 'placeholder' => 'Enter Employee Designation')); }}  
+            'form-control', 'placeholder' => 'Enter Employee Designation')); }}
             <span class='errorlogin email-login'>{{$errors->first('designation');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
-    
+
     <div class="form-group">
         {{ Form::label('roles', 'Roles: ', array('class' => 'col-sm-3
         control-label')); }}
-        <div class="col-sm-8">{{ Form::select('roles', $roles, null, array('class' => 'form-control')) }} 
+        <div class="col-sm-8">{{ Form::select('roles', $roles, null, array('class' => 'form-control')) }}
             <span class='errorlogin email-login'>{{$errors->first('roles');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
@@ -50,22 +50,22 @@
     <div class="form-group">
         {{ Form::label('phone_no', 'Phone: ', array('class' => 'col-sm-3
         control-label')); }}
-        <div class="col-sm-8">{{ Form::text('phone_no', "", array('class' => 'form-control', 'placeholder' => 'ex. (704) 888-9999', "data-inputmask"=>'"mask": "(999) 999-9999"', "data-mask")); }} 
+        <div class="col-sm-8">{{ Form::text('phone_no', "", array('class' => 'form-control', 'placeholder' => 'ex. (704) 888-9999', "data-inputmask"=>'"mask": "(999) 999-9999"', "data-mask")); }}
             <span class='errorlogin email-login'>{{$errors->first('phone_no');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('gender', 'Gender: ', array('class' => 'col-sm-3
         control-label')); }}
-        <div class="col-sm-8">{{ Form::select('gender', array('Male', 'Female'), null, array('class' => 'form-control')) }} 
+        <div class="col-sm-8">{{ Form::select('gender', array('Female', 'Male'), null, array('class' => 'form-control')) }} 
             <span class='errorlogin email-login'>{{$errors->first('gender');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
-    
+
     <div class="form-group">
         {{ Form::label('country_id', 'Country: ', array('class' => 'col-sm-3
         control-label')); }}
-        <div class="col-sm-8">{{ Form::select('country_id', $country, null, array('class' => 'form-control')) }} 
+        <div class="col-sm-8">{{ Form::select('country_id', $country, null, array('class' => 'form-control')) }}
             <span class='errorlogin email-login'>{{$errors->first('country_id');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
@@ -74,7 +74,7 @@
         {{ Form::label('Password', 'Password: ', array('class' => 'col-sm-3
         control-label')); }}
         <div class="col-sm-8">
-            {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Enter Employee Password')); }} 
+            {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Enter Employee Password')); }}
             <span class='errorlogin password-login'>{{$errors->first('password');}}</span>
         </div>
     </div>
@@ -83,14 +83,14 @@
         {{ Form::label('Confirm Password', 'ConfirmPassword: ', array('class' => 'col-sm-3
         control-label')); }}
         <div class="col-sm-8">
-            {{ Form::password('confirm_password', array('class' => 'form-control', 'placeholder' => 'Confirm Password')); }} 
+            {{ Form::password('confirm_password', array('class' => 'form-control', 'placeholder' => 'Confirm Password')); }}
             <span class='errorlogin password-login'>{{$errors->first('confirm_password');}}</span>
         </div>
     </div>
     <div class="form-group" id="mentor_id_view" style="display:none;">
         {{ Form::label('mentor_id', 'Mentor: ', array('class' => 'col-sm-3
         control-label')); }}
-        <div class="col-sm-8">{{ Form::select('mentor_id', array("", 'Select Your Mentor'), '', array('class' => 'form-control')) }} 
+        <div class="col-sm-8">{{ Form::select('mentor_id', array("", 'Select Your Mentor'), '', array('class' => 'form-control')) }}
             <span class='errorlogin email-login'>{{$errors->first('mentor_id');}}@if(!empty($message)){{$message}}@endIf</span>
         </div>
     </div>
